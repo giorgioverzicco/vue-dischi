0f44775adfb9a538537995571ffcbd2804d8fcce
giorgioverzicco <giorgioverzicco00@gmail.com>
2022-06-07 13:20:06 +0200

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+  content: ["./public/index.html", "./src/**/*.{vue,js}"],
+  theme: {
+    extend: {
+      colors: {
+        "slate-blue": {
+          500: "#2E3A46",
+          700: "#1E2D3B",
+        },
+      },
+    },
+  },
+  plugins: [],
+};
